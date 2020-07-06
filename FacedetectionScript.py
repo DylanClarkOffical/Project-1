6a0a0f1144091d2e40831ca4764268d8323bd9bb
DylanClarkOffical <47819419+DylanClarkOffical@users.noreply.github.com>
2020-07-06 17:33:34 +0200

    updated some stuff
    ## added IP CAM ability 
    ## added more comments

--- FacedetectionScript.py
+++ FacedetectionScript.py
@@ -1,57 +1,48 @@
-import cv2 
-import numpy as np
-import sys
+## TODO: implement Exception handling
+## TODO: Implement more Cascades  for different objects
+## TODO: optimize code Etc
+## TODO: implement a GUI
+## TODO: implement custom android app for IP WEBCAM
+
+## imports 
+import cv2  # OpenCV lib
 import logging as log
 import datetime as dt
-from time import sleep
 
-cascPath = "haarcascade_frontalface_default.xml"
+cascPath = 'FaceCascadeClassifier.xml'
 faceCascade = cv2.CascadeClassifier(cascPath)
 log.basicConfig(filename='webcam.log',level=log.INFO)
 anterior = 0
 
-url = 'ENTER ADDRESS FROM IP CAM HERE'
-cap = cv2.VideoCapture(url)
-video_capture = cap
+url = '		' #url from the IP WEBCAM android application
+video_capture = cv2.VideoCapture(url) # pass the url into the CV Capture into  cv2.VideoCapture(url) then give it a varible for later use 
 
 while(True):
-    # Capture frame-by-frame
-    ret, frame = video_capture.read()
-    
-   ## conver to gray 
-    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
+    ret, frame = video_capture.read() # Capture frame-by-frame
+    gray = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY) # convert the frames to Gray 
 
-    faces = faceCascade.detectMultiScale(
+    faces = faceCascade.detectMultiScale( # this is apart of the detection algorithm 
         gray,
-        scaleFactor=1.1,
-        minNeighbors=5,
-    #dection size 
-        minSize=(100,100) 
+        scaleFactor=1.1, #Parameter specifying how much the image size is reduced at each image scale.
+        minNeighbors=35, #Parameter specifying how many neighbors each candidate rectangle should have to retain it.
+        minSize=(80,80) #Minimum possible object size. Objects smaller than that are ignored.
     )
 
-    # Draw a rectangle around the faces
-    ## faces that are found in the faceCascade
+    # Draw a rectangle around the faces at position  (x, y, w, h)
+   
     for (x, y, w, h) in faces:
         cv2.rectangle(frame, (x, y), (x+w, y+h), (102,255,0), 2)
-    #logging purposes 
-    ## logs how many faces are detected 
-    ## and at what date and time it was detected
+	#logging purposes 
     if anterior != len(faces):
         anterior = len(faces)
-        log.info("faces: "+str(len(faces))+" at "+str(dt.datetime.now()))
+        log.info("Number of faces detected: "+str(len(faces))+" at this Date and time: "+str(dt.datetime.now()))
 
     # Display the resulting frame
     cv2.imshow('Video', frame)
-
-    ## press Q on the keyboard to exit the application 
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-        
-    # Display the resulting frame
-    cv2.imshow('Video', frame)
-## destroy and release 
+    #exit the program by pressing q 
+    q = cv2.waitKey(1)
+    if q == ord("q"):
+     print ('program has exited')
+     break
 cv2.destroyAllWindows()
 video_capture.release()
-
-
-
